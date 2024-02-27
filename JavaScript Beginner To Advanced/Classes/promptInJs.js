61ccfc85bdf2cd48f09af9c0ba6a6a05acf8ca04
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-02-27 20:11:33 +0500

    prompt In Js

---
+++ JavaScript Beginner To Advanced/Classes/promptInJs.js
@@ -0,0 +1,11 @@
+// var firstName = prompt("Enter Your First Name", "John");
+// var lastName = prompt("Enter Your Lasr Name", "Doe");
+
+// document.write("Hello " + firstName + " " + lastName + " Nice to Meet You.");
+console.log(alert);
+
+Swal.fire({
+  title: "Good job!",
+  text: "You clicked the button!",
+  icon: "success",
+});
