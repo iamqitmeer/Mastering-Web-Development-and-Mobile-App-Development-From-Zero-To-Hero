251af5c7221ee24be9cb4285868aa4a8a49bf450
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-02 23:59:17 +0500

    Questions

--- JavaScript Beginner To Advanced/Practice/AllPracticeQuestion.js
+++ JavaScript Beginner To Advanced/Practice/AllPracticeQuestion.js
@@ -1,25 +1,94 @@
 // Ek prompt se user se ek number input lekar, agar number 10 se bada hai toh "Bahut bada number hai" warna "Chhota number hai" alert karo.
 
-
-var userInput = prompt('Enter Any Number.');
-if (condition) {
-  
-}
-
+// var userInput = prompt("Enter Any Number.");
+// if (userInput > 10) {
+//   alert(`${userInput} --> Bohot Bara Number Hai.`);
+// } else {
+//   alert(`${userInput} --> Bohot Chota Number Hai.`);
+// }
 
 // Ek prompt se user se do strings input lo. Agar dono strings ka length bara hai toh "Dono strings ka length bara hai" warna "Dono strings ka length bara nahi hai" alert karo.
 
+// var userInput1 = prompt("Enter First Input Value.");
+// var userInput2 = prompt("Enter Second Input Value.");
+
+// if (userInput1.length < userInput2.length) {
+//   alert("Your First Input Lenght Is Small Then Second Input.");
+// } else if (userInput1.length == userInput2.length) {
+//   alert("Your First Input Lenght Is Equal to Second Input.");
+// } else {
+//   alert("Your First Input Lenght Is Bigger Then Second Input.");
+// }
+
 // Ek prompt se user se do numbers input lo. Agar pehla number doosre number se bara hai toh "Pehla number bara hai" warna "Pehla number bara nahi hai" alert karo.
+
+// var userInput1 = +prompt("Enter 1st Number.");
+// var userInput2 = +prompt("Enter 2nd Number.");
+
+// if (userInput1 < userInput2) {
+//   alert("Your First Input Is Small Then Second Input.");
+// } else if (userInput1 == userInput2) {
+//   alert("Your First Input Is Equal to Second Input.");
+// }else if (userInput1 == userInput2) {
+//   alert("Your First Input Is Equal to Second Input.");
+// } else {
+//   alert("Your First Input Is Bigger Then Second Input.");
+// }
 
 // Ek prompt se user se ek number input lekar, agar number 5 se chhota hai toh uska square print karo, agar 5 se bara hai toh uska cube print karo.
 
+// var userInput = +prompt("Enter Any Number.");
+// if (userInput <= 5) {
+//   alert(Math.pow(userInput, 2));
+// } else {
+//   alert(Math.pow(userInput, 3));
+// }
+
 // Ek prompt se user se ek number input lekar, agar number 2 se divisible hai toh "Divisible by 2" warna "Not divisible by 2" alert karo.
+
+// var userInput = +prompt("Enter Any Number.");
+// if (userInput % 2) {
+//   alert("Not Divisable By 2");
+// } else {
+//   alert("Divisable By 2");
+// }
 
 // Ek prompt se user se do numbers input lo. Agar pehla number 2 se chhota hai aur doosra number 5 se bara hai toh "Conditions satisfied" warna "Conditions not satisfied" alert karo.
 
+// var userInput1 = +prompt("Enter 1st Number.");
+// var userInput2 = +prompt("Enter 2nd Number.");
+
+// if (userInput1 < 2 && userInput2 > 5) {
+//   alert('Conditions satisfied')
+// } else {
+//   alert('Conditions not satisfied')
+// }
+
 // Ek prompt se user se ek number input lekar, agar number 0 se bara hai toh "Positive" alert karo, agar 0 hai toh "Zero" alert karo, aur agar negative hai toh "Negative" alert karo.
 
+// var userInput = +prompt("Enter Any Number.");
+
+// if (userInput > 0) {
+//   alert('Possitive')
+// }else if (userInput == 0) {
+//   alert('Zero')
+// } else {
+//   alert('Negative')
+// }
+
 // Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka product print karo, agar pehla number bara hai toh uska square print karo, aur agar doosra number bara hai toh uska square root print karo.
+
+var userInput1 = +prompt("Enter 1st Number.");
+var userInput2 = +prompt("Enter 2nd Number.");
+
+if (userInput1 === userInput2) {
+  alert(userInput1 * userInput2)
+} else if(userInput1 > userInput2) {
+  var sqValue = userInput1 * userInput2
+  alert(Math.pow(sqValue, 2))
+} else{
+
+}
 
 // Ek prompt se user se ek string input lekar, agar wo "Hello" hai toh "Welcome" alert karo, agar "World" hai toh "Goodbye" alert karo, aur agar kuch aur hai toh "Unknown input" alert karo.
 
@@ -64,4 +133,3 @@ if (condition) {
 // Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka sum print karo, agar pehla number bara hai toh uska cube print karo, aur agar doosra number chhota hai toh uska square root print karo.
 
 // Ek prompt se user se ek string input lo. Agar string ka length 10 se bara hai toh "Length is greater than 10", agar 10 hai toh "Length is equal to 10", aur agar 10 se chhota hai toh "Length is less than 10" alert karo.
-
