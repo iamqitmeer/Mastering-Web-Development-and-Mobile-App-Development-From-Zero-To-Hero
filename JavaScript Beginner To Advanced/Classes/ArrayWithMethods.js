6420fbda600743083c8d7e99be9f55c5b2e9c650
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-05 13:04:49 +0500

    Array

---
+++ JavaScript Beginner To Advanced/Classes/ArrayWithMethods.js
@@ -0,0 +1,16 @@
+var myFamily = ["Iqbal", "Zetoon", "Qitmeer", "Moavia", "Inam", "Zareena"];
+// console.log(myFamily);
+// console.log(myFamily[0]);
+// console.log(myFclamily[1]);
+// console.log(myFamily[2]);
+// console.log(myFamily[3]);
+// console.log(myFamily[4]);
+// console.log(myFamily[5]);
+
+// Pop
+
+// var retunArray = myFamily.unshift("Safia");
+
+var retunArray = myFamily.slice(1, 2);
+console.log(myFamily);
+// console.log(retunArray);
--- JavaScript Beginner To Advanced/Practice/AllPracticeQuestion.js
+++ JavaScript Beginner To Advanced/Practice/AllPracticeQuestion.js
@@ -79,58 +79,38 @@
 
 // Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka product print karo, agar pehla number bara hai toh uska square print karo, aur agar doosra number bara hai toh uska square root print karo.
 
-var userInput1 = +prompt("Enter 1st Number.");
-var userInput2 = +prompt("Enter 2nd Number.");
+// var userInput1 = +prompt("Enter 1st Number.");
+// var userInput2 = +prompt("Enter 2nd Number.");
 
-if (userInput1 === userInput2) {
-  alert(userInput1 * userInput2)
-} else if(userInput1 > userInput2) {
-  var sqValue = userInput1 * userInput2
-  alert(Math.pow(sqValue, 2))
-} else{
-
-}
+// if (userInput1 === userInput2) {
+//   alert(userInput1 * userInput2);
+// } else if (userInput1 > userInput2) {
+//   var sqValue = userInput1 * userInput2;
+//   alert(Math.pow(sqValue, 2));
+// } else {
+// }
 
 // Ek prompt se user se ek string input lekar, agar wo "Hello" hai toh "Welcome" alert karo, agar "World" hai toh "Goodbye" alert karo, aur agar kuch aur hai toh "Unknown input" alert karo.
 
-// Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka sum print karo, agar pehla number bara hai toh uska cube print karo, aur agar doosra number bara hai toh uska square root print karo.
-
-// Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh "Numbers are equal", agar pehla number doosre se bara hai toh "First number is greater", aur agar doosra number pehle se bara hai toh "Second number is greater" alert karo.
+// var userInput = prompt("Enter Any Word.");
+// if (userInput === "Hello") {
+//   alert("Welcome");
+// } else if (userInput === "World") {
+//   alert("GoodBye");
+// } else {
+//   alert("Unknown input");
+// }
 
 // Ek prompt se user se ek number input lo. Agar number 10 se chhota hai aur 20 se bara hai toh "Number is between 10 and 20", agar 10 ya 20 hai toh "Number is equal to 10 or 20", aur agar in dono ke beech nahi hai toh "Number is not between 10 and 20" alert karo.
 
-// Ek prompt se user se ek string input lo. Agar string "yes" ya "YES" hai toh "User input is affirmative", agar "no" ya "NO" hai toh "User input is negative", aur agar kuch aur hai toh "Invalid input" alert karo.
+// var userInput = +prompt("Enter Number.");
+// if (userInput >= 10 && userInput <= 20) {
+//   alert("Number is Between 10 & 20.");
+// } else if (userInput < 10 && userInput > 0) {
+//   alert("Number is Less Than 10");
+// } else if (userInput > 20 && userInput < 100) {
+//   alert("Number is Greater Than 10");
+// } else {
+//   alert("Unknown Number");
+// }
 
-// Ek prompt se user se ek number input lo. Agar number 100 se bara hai toh uska square root print karo, agar 50 se bara hai toh uska square print karo, aur agar 20 se bara hai toh uska cube print karo.
-
-// Ek prompt se user se ek string input lo. Agar string ka length 5 se chhota hai toh uski first half print karo, agar 5 se bara hai toh uski second half print karo.
-
-// Ek prompt se user se ek number input lo. Agar number 1 se chhota hai toh "Number is less than 1", agar 1 hai toh "Number is equal to 1", aur agar 1 se bara hai toh "Number is greater than 1" alert karo.
-
-// Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka square sum print karo, agar pehla number bara hai toh uska square root print karo, aur agar doosra number bara hai toh uska cube print karo.
-
-// Ek prompt se user se ek string input lo. Agar string me "hello" shamil hai toh "Greeting detected", agar "goodbye" shamil hai toh "Farewell detected", aur agar dono nahi hain toh "No recognizable greeting or farewell" alert karo.
-
-// Ek prompt se user se ek number input lo. Agar number 100 se zyada hai ya 50 se kam hai toh uska square print karo, agar 100 se chhota hai ya 50 se bara hai toh uska cube print karo.
-
-// Ek prompt se user se do strings input lo. Agar dono strings ke length barabar hai toh unki concatenation print karo, aur agar unka length alag hai toh "Lengths are not equal" alert karo.
-
-// Ek prompt se user se ek number input lo. Agar number 10 se bara hai aur 20 se chhota hai toh "Number is between 10 and 20", agar 10 ya 20 hai toh "Number is either 10 or 20", aur agar in dono ke beech nahi hai toh "Number is not between 10 and 20" alert karo.
-
-// Ek prompt se user se ek string input lo. Agar string me "javascript" shamil hai toh "JavaScript detected", agar "python" shamil hai toh "Python detected", aur agar dono nahi hain toh "No recognizable programming language" alert karo.
-
-// Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka product print karo, agar pehla number chhota hai toh uska square print karo, aur agar doosra number bara hai toh uska square root print karo.
-
-// Ek prompt se user se ek string input lo. Agar string "hello" ya "Hello" hai toh "Greeting detected", agar "bye" ya "Bye" hai toh "Farewell detected", aur agar kuch aur hai toh "No recognizable greeting or farewell" alert karo.
-
-// Ek prompt se user se ek number input lo. Agar number 10 se zyada hai aur 20 se kam hai toh "Number is between 10 and 20", agar 10 ya 20 hai toh "Number is equal to 10 or 20", aur agar in dono ke beech nahi hai toh "Number is not between 10 and 20" alert karo.
-
-// Ek prompt se user se do strings input lo. Agar dono strings ka length barabar hai toh unki reverse concatenation print karo, aur agar unka length alag hai toh "Lengths are not equal" alert karo.
-
-// Ek prompt se user se ek number input lo. Agar number 100 se bara hai toh "Number is greater than 100", agar 50 se kam hai toh "Number is less than 50", aur agar in dono ke beech hai toh "Number is between 50 and 100" alert karo.
-
-// Ek prompt se user se ek string input lo. Agar string me "JavaScript" shamil hai toh "Programming language detected", agar "HTML" shamil hai toh "Markup language detected", aur agar dono nahi hain toh "No recognizable language detected" alert karo.
-
-// Ek prompt se user se do numbers input lo. Agar dono numbers equal hain toh unka sum print karo, agar pehla number bara hai toh uska cube print karo, aur agar doosra number chhota hai toh uska square root print karo.
-
-// Ek prompt se user se ek string input lo. Agar string ka length 10 se bara hai toh "Length is greater than 10", agar 10 hai toh "Length is equal to 10", aur agar 10 se chhota hai toh "Length is less than 10" alert karo.
