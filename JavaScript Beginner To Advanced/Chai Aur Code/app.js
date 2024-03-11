b0f621afb266651671ec17990fbcf981740e752c
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-11 16:31:50 +0500

    learning with chai aur code

---
+++ JavaScript Beginner To Advanced/Chai Aur Code/app.js
@@ -0,0 +1,2 @@
+// let myName = "Qitmeer";
+// let age = 15;
--- JavaScript Beginner To Advanced/Classes/ArrayWithMethods.js
+++ JavaScript Beginner To Advanced/Classes/ArrayWithMethods.js
@@ -7,10 +7,22 @@ var myFamily = ["Iqbal", "Zetoon", "Qitmeer", "Moavia", "Inam", "Zareena"];
 // console.log(myFamily[4]);
 // console.log(myFamily[5]);
 
-// Pop
+// var retunArray = myFamily.slice(-3);
+// console.log(retunArray);
 
-// var retunArray = myFamily.unshift("Safia");
+var retunArray = myFamily.push("Safia");
+console.log(retunArray);
 
-var retunArray = myFamily.slice(1, 2);
-console.log(myFamily);
+// var retunArray = myFamily.slice(-3);
+// console.log(retunArray);
+
+// var retunArray = myFamily.slice(-3);
+// console.log(retunArray);
+
+// var retunArray = myFamily.slice(-3);
+// console.log(retunArray);
+
+// var retunArray = myFamily.slice(-3);
+// console.log(retunArray);
+
 // console.log(retunArray);
