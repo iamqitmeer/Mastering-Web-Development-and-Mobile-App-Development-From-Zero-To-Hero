9c9fa579e35cbdf1d3eba4738dc7ed3cc31c0285
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-11 19:23:29 +0500

    comparison

---
+++ JavaScript Beginner To Advanced/Chai Aur Code/comparison.js
@@ -0,0 +1,7 @@
+console.log(1 > 0);
+console.log(5 < 4);
+console.log(2 <= 3);
+console.log(1 == 1);
+console.log(1 == 0);
+console.log(1 !== 0);
+console.log(1 > 0);
---
+++ JavaScript Beginner To Advanced/Chai Aur Code/datatype.js
@@ -0,0 +1,33 @@
+// ---------------------- Datatype ---------------------- \\
+
+// There Are two Type of Datatypes
+// 1. Primitive
+// 2. Non-Primitive
+
+// Primitive:
+
+// --- Primitive Data Types: Inme primitive values store hoti hain, jo directly variable ke memory space mein store hoti hain.
+// --- Immutable: Primitive values immutable hote hain, matlab unki value ek baar set hone ke baad badal nahi sakti.
+// --- Pass by Value: Jab primitive values ko functions mein pass kiya jata hai, toh woh value ke saath hi pass hoti hai.
+// --- Direct Access: Primitive values ko variable ke through directly access kiya ja sakta hai.
+
+// Data Types:
+
+// Number: Integer, floating-point numbers, aur special numeric values (NaN, Infinity, -Infinity).
+// String: Ek sequence of characters.
+// Boolean: Logical values true ya false.
+// Null: Intentional absence of any object value.
+// Undefined: Variable declared hai lekin usmein koi value assign nahi ki gayi hai.
+// Symbol: Unique identifiers, introduced in ECMAScript 6 (ES6).
+
+// Non-Primitive
+// -- Ye complex data types hote hain aur reference values ko store karte hain.
+// -- Ye mutable (changeable) hote hain.
+
+// Non-primitive data types include:
+
+// Object (ऑब्जेक्ट): Key-value pairs ke collection ko represent karne ke liye. Jaise { name: "John", age: 30 }
+// Array (एरे): Ordered collection of values ko store karne ke liye, jaise [1, 2, 3, 4, 5]
+// Function (फंक्शन): Code block ko reusability ke liye define karne ke liye.
+// Date (डेट): Date aur time ko represent karne ke liye.
+// Any other object, jaise Map, Set, Error, etc.
