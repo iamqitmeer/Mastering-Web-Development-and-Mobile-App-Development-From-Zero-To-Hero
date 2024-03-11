a6e5ecd3cd413a422aab6d8ae8722f98b8b4ad6e
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-11 17:03:07 +0500

    Varieble Declaration

---
+++ JavaScript Beginner To Advanced/Chai Aur Code/varieble.js
@@ -0,0 +1,25 @@
+// ---------------------- Varieble Declaration ---------------------- \\
+
+// var myName = "Qitmeer";
+// console.log(myName);
+
+// -- var pichlay JavaScript versions mein standard tareeqa tha variables declare karne ka.
+// -- Iska scope function-level ho sakta hai ya global, lekin agar var ko block mein declare kiya gaya hai, toh uska scope us block tak mehdood hota hai.
+// --  var ki value baad mein bhi change ki ja sakti hai.
+// -- var ko re-declare kiya ja sakta hai.
+
+// let myFather = "Iqbal";
+// console.log(myFather);
+
+// -- let ECMAScript 6 (ES6) ke saath aaya aur var ki kuch limitations ko hal karta hai.
+// -- let ka scope block-level hota hai, jo ki us block ke andar hi mehdood hota hai.
+// -- let ki value baad mein bhi change ki ja sakti hai.
+// -- let ko re-declare nahi kiya ja sakta.
+
+// const myBro = "Inam";
+// console.log(myBro);
+
+// -- const bhi ECMAScript 6 (ES6) ke saath aaya hai.
+// -- const ka scope block-level hota hai.
+// -- const variables ki value ek baar assign hone ke baad badal nahi sakti.
+// -- const ko re-declare nahi kiya ja sakta aur ek baar value assign kar di gayi hai, woh badal nahi sakti.
