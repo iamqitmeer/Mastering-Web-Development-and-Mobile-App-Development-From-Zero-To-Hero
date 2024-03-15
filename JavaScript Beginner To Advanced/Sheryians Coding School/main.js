2c62c274695e9bc0c284d63f57ff456a866c2e52
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-15 15:07:06 +0500

    Notes - Sheryians Coding School

---
+++ JavaScript Beginner To Advanced/Sheryians Coding School/main.js
@@ -0,0 +1,30 @@
+// Single - Topic
+
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+
+// word vs keyword
+// word - aisi cheez ka jis ka matlab js me na ho called word
+// keyword - aisi cheez jis ka matlab js me hoo called keyword
+
+// what is variebles
+// varieble is container jis me ham data ko store kr skte he.
+
+// varieble creation
+myName = "Qitmeer Raza"; // ye varieble he lekin aise varieble banana ghalat he. ye bilkul bhi achi practice nhi he.
+
+// var vs let vs const
+
+// var -- function scope rakhta hai aur use dubara declare aur reassign kiya ja sakta hai.
+// var myName = "Qitmeer Raza";
+
+// let -- block scope rakhta hai aur use reassign kiya ja sakta hai lekin dubara declare nahi kiya ja sakta.
+// let myName = "Qitmeer Raza";
+
+// const -- block scope rakhta hai aur initialization ke baad reassign ya dubara declare nahi kiya ja sakta.
+// const myName = "Qitmeer Raza";
+
+// hoisting
+// Hoisting mein, JavaScript engine variable aur function declarations ko code ke shuru mein le jaata hai, matlab ki unhe upar laata hai, lekin actual assignment ya initialization ko nahi.
