c3d00aecd057be0a6f75fdad8b10fb9aa9b1b58e
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-22 06:33:53 +0500

    Questions Solved

--- JavaScript Beginner To Advanced/Book Assigment Solved/5thChapter.js
+++ JavaScript Beginner To Advanced/Book Assigment Solved/5thChapter.js
@@ -31,5 +31,105 @@ var number = 12;
 number = 15;
 // console.log(`Value After Varieble Initialization is --- ${number}`);
 
-number = number + 1;
-console.log(number)
+// number = number + 1;
+// console.log(`Value After Increament is --- ${number}`);
+
+// number = number + 7;
+// console.log("Value --", number);
+
+// console.log("After Add 7 - The Value is", number);
+// console.log(number)
+
+// number = number--;
+// console.log(`Value After Deccreament is --- ${number}`);
+
+// number = number % 3;
+// console.log("Value Reminder is -- ", number);
+
+// -------------------------------------------------------------------------------------
+
+// var ticketPrice = 500;
+// console.log(`Total Amount of Buying 5 Ticket - `, ticketPrice * 5);
+
+// -------------------------------------------------------------------------------------
+
+// var numOfTable = 4;
+// var num = 0;
+
+// document.write(`<b> Table Of ${numOfTable} </b>`);
+// document.write(
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>",
+//   numOfTable + "+" + ++num + "=" + numOfTable * num + "<br>"
+// );
+
+// -------------------------------------------------------------------------------------
+
+// var item1Price = 700;
+// var item2Price = 1200;
+// var quantityOfItem1 = 2;
+// var quantityOfItem2 = 4;
+// var shippingCharges = 200;
+
+// document.write(`Price of Item 1 is - ${item1Price}`);
+// document.write("<br>");
+// document.write(`Quantity Of Item1 is ${quantityOfItem1}`);
+// document.write("<br>");
+// document.write(`Price of Item 2 is - ${item2Price}`);
+// document.write("<br>");
+// document.write(`Quantity Of Item2 is ${quantityOfItem2}`);
+// document.write("<br>");
+// document.write(`Shipping Charges - ${shippingCharges}`);
+// document.write("<br>");
+
+// var totalItem1 = item1Price * quantityOfItem1 + shippingCharges;
+// var totalItem2 = item2Price * quantityOfItem2 + shippingCharges;
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write(`Total Price Of Item 1 With Delivery ${totalItem1}`);
+// document.write("<br>");
+// document.write(`Total Price Of Item 2 With Delivery ${totalItem2}`);
+
+// -------------------------------------------------------------------------------------
+
+// var mathMarks = 60;
+// var physicsMarks = 75;
+// var bioMarks = 67;
+// var urduMarks = 53;
+// var chemistryMarks = 62;
+// var islamiatMarks = 97;
+
+// var obtainedMarks = 600;
+
+// document.write(
+//   "mathMarks - " +
+//     mathMarks +
+//     "<br>" +
+//     "physicsMarks - " +
+//     physicsMarks +
+//     "<br>" +
+//     "bioMarks - " +
+//     bioMarks +
+//     "<br>" +
+//     "islamiatMarks - " +
+//     islamiatMarks +
+//     "<br>" +
+//     "urduMarks - " +
+//     urduMarks +
+//     "<br>"
+// );
+
+// document.write("<br>");
+// var abc = mathMarks + urduMarks + islamiatMarks + bioMarks + chemistryMarks;
+
+// document.write(`Total Marks Out Of ${obtainedMarks} is ${abc}`);
+// document.write("<br>");
+// document.write(`Total Percentage is ${(abc * 100) / obtainedMarks}`);
