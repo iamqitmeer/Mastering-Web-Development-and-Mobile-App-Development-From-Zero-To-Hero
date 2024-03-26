aacea03e36636def19c3cd49878762d118ca34df
Qitmeer Raza <qitmeeerrrazaa@gmail.com>
2024-03-26 16:36:53 +0500

    spreadOperator

---
+++ JavaScript Beginner To Advanced/Book Assigment Solved/12thTo13thChapter.js
@@ -0,0 +1,85 @@
+// -------------------------------------------------------------------------------------
+
+// var userInput = prompt("Enter Something");
+
+// if (typeof userInput == "string") {
+//   console.log(`${userInput} -- is String`);
+// } else if (typeof userInput == "number") {
+//   console.log(`${userInput} -- is Number`);
+// } else if (typeof userInput == "boolean") {
+//   console.log(`${userInput} -- is Number`);
+// } else
+// if (userInput.codePointAt() <= 97) {
+//   console.log(`${userInput} -- is UpperCase`);
+// } else if (userInput.codePointAt() >= 97) {
+//   console.log(`${userInput} -- is lowerCase`);
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var userInput1 = prompt("Enter Something");
+// var userInput2 = prompt("Enter Something");
+
+// if(userInput1 > userInput2){
+//   console.log(`First Is Larger As Compare to 2nd`)
+// } else if(userInput1 < userInput2){
+//   console.log(`2nd Is Larger As Compare to 1st`)
+// } else if(userInput1 = userInput2){
+//   console.log(`Both Prompt is Equal`)
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var userInput1 = prompt("Enter Something");
+
+// if (userInput1[0] == "-") {
+//   console.log(`Enter Negative Number`);
+// } else if (userInput1 == 0) {
+//   console.log(`Enter 0 Number`);
+// } else if (userInput1 != "-") {
+//   console.log(`Enter Positive Number`);
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var userInput = prompt("Enter Something");
+
+// if (
+//   userInput === "e" ||
+//   userInput === "o" ||
+//   userInput === "i" ||
+//   userInput === "a" ||
+//   userInput === "u"
+// ) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var psw = "01234567";
+// var userInput = prompt("Please enter your password");
+// var userInput1 = prompt("Please confirm your password");
+
+// if (userInput !== userInput1) {
+//   console.log("Password Does not Match");
+// } else if (userInput == psw && userInput1 == psw) {
+//   console.log("Correct!");
+// } else {
+//   console.log("Incorrect password");
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var greeting;
+// var hour = 19;
+// if (hour < 18) {
+//   greeting = "Good day";
+// } else {
+//   greeting = "Good evening";
+// }
+
+// console.log(greeting);
+
+// -------------------------------------------------------------------------------------
--- JavaScript Beginner To Advanced/Book Assigment Solved/9thTo11thChapter.js
+++ JavaScript Beginner To Advanced/Book Assigment Solved/9thTo11thChapter.js
@@ -9,3 +9,124 @@
 //   );
 // }
 
+// -------------------------------------------------------------------------------------
+
+// var userInput = prompt("Enter Your Gender.", "Male");
+// userInput.toLocaleLowerCase();
+
+// if (userInput == "male") {
+//   console.log("Welcome Sir");
+// } else {
+//   console.log("Welcome Madam");
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var userInput = prompt("Enter Traffic Light Color.", "Red");
+// userInput.toLocaleLowerCase();
+
+// if (userInput == "red") {
+//   console.log("Must Stop");
+// } else if (userInput == "green") {
+//   console.log("ready to move");
+// } else {
+//   console.log("move now");
+// }
+
+// -------------------------------------------------------------------------------------
+
+// if ("car" < "cat") {
+//   alert("True");
+// } else {
+//   alert("False");
+// }
+
+// Ye code true return kar raha hai kyunki JavaScript me "car" aur "cat" strings ko compare karte waqt lexicographical order ka use hota hai. Matlab "car" string ke characters "cat" ke characters se pehle aate hain, kyonki 'r' chota hota hai 't' se, isliye "car" < "cat" true hai. Jaise hi condition true hoti hai, "True" alert show ho jaata hai.
+
+// -------------------------------------------------------------------------------------
+
+// var obtMarks = prompt("Enter Your Obt Marks.");
+
+// document.write(`
+// <h1>Mark Sheet</h1>
+
+// <h3>300</h3>
+// <h3>${obtMarks}</h3>
+// <h3>${(obtMarks * 100) / 300}%</h3>
+// `);
+
+// if (obtMarks >= 240 && obtMarks <= 300) {
+//   document.write(`<h3>A</h3>`);
+// } else if (obtMarks >= 200 && obtMarks < 240) {
+//   document.write(`<h3>B</h3>`);
+// } else if (obtMarks >= 140 && obtMarks < 200) {
+//   document.write(`<h3>C</h3>`);
+// } else {
+//   document.write(`<h3>Fail</h3>`);
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var guessedNumber = 2;
+// var userInput = prompt("Enter Number");
+
+// if (userInput == guessedNumber) {
+//   console.log("Correct");
+// } else if (userInput == ++guessedNumber) {
+//   console.log("Closed");
+// } else {
+//   console.log("Wrong");
+// }
+
+// -------------------------------------------------------------------------------------
+
+// const userInput = prompt("Enter a number:");
+// if (userInput % 3 === 0) {
+//   console.log(`${userInput} is divisible by 3`);
+// } else {
+//   console.log(`${userInput} is not divisible by 3`);
+// }
+
+// -------------------------------------------------------------------------------------
+
+// const userInput = prompt("Enter a number:");
+// if (userInput % 2 === 0) {
+//   console.log(`${userInput} is Even Number`);
+// } else {
+//   console.log(`${userInput} is Odd Number`);
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var temp = prompt("Enter Temprature");
+
+// if (temp > 40) {
+//   console.log("Oh! It is Hot too Outside");
+// } else if (temp > 30) {
+//   console.log("The Weather Today is Normal");
+// } else if (temp > 20) {
+//   console.log("Today Weather is Cool");
+// } else if (temp > 10) {
+//   console.log("OMG1 Today Weather is Cool");
+// } else {
+//   console.log("Error");
+// }
+
+// -------------------------------------------------------------------------------------
+
+// var userInput1 = +prompt("Enter First Number");
+// var userInput2 = +prompt("Enter 2nd Number");
+
+// var userInput3 = prompt("which operation do you want to perform");
+
+// if (userInput3 == "+") {
+//   console.log(userInput1 + userInput2);
+// } else if (userInput3 == "*") {
+//   console.log(userInput1 * userInput2);
+// } else if (userInput3 == "/") {
+//   console.log(userInput1 / userInput2);
+// } else if (userInput3 == "-") {
+//   console.log(userInput1 - userInput2);
+// } else if (userInput3 == "%") {
+//   console.log(userInput1 % userInput2);
+// }
